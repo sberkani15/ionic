96a095f059fe67b7e52b5bf6878450edddbfe6b2
sberkani <slimane.berkani15@gmail.com>
2019-01-09 23:56:16 +0100

    app update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather: any;
+  location : {
+    city: string,
+    state: string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _weatherProvider: WeatherProvider, private _storage: Storage ) {
 
   }
+
+  ionViewWillEnter () {
+    this._storage.get('location').then(
+      val => {
+        if (val !== null) {
+          this.location = JSON.parse(val);
+        } else {
+          this._storage.set('location', JSON.stringify({city: 'Miami', state: 'FL'}))
+          this.location = {city: 'Miami', state: 'FL'};
+        }
+        this._weatherProvider.getWeather(this.location.city, this.location.state).subscribe(
+      (data: any) => (
+        this.weather = data.current_observation
+        )
+    );
+      }
+    )
+    
+  }
+
+  
 
 }
